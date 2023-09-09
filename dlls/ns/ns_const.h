084c76ab00b8ebba1774460b16999a06a2f47d45
pierow <pierow8@gmail.com>
2023-09-09 07:24:35 -0400

    NS 3.3b6 offsets

--- dlls/ns/ns_const.h
+++ dlls/ns/ns_const.h
@@ -61,53 +61,53 @@
 #define	OFFSET_WIN_WEAP_NEXT		320
 #define	OFFSET_LIN_WEAP_NEXT		336
 
-#define OFFSET_WIN_HIVE_TRAIT		488
-#define OFFSET_LIN_HIVE_TRAIT		504
+#define	OFFSET_WIN_HIVE_TRAIT		512		//488
+#define	OFFSET_LIN_HIVE_TRAIT		528		//504
 
-#define OFFSET_WIN_SCORE			6588
-#define OFFSET_LIN_SCORE			6608
+#define	OFFSET_WIN_SCORE			6636	//6588
+#define	OFFSET_LIN_SCORE			6736	//6608
 
-#define OFFSET_WIN_EXP				6512
-#define OFFSET_LIN_EXP				6532
+#define	OFFSET_WIN_EXP				6568	//6512
+#define	OFFSET_LIN_EXP				6636	//6532
 
-#define OFFSET_WIN_POINTS			6520
-#define OFFSET_LIN_POINTS			6540
+#define	OFFSET_WIN_POINTS			6576	//6520
+#define	OFFSET_LIN_POINTS			6644	//6540
 
-#define OFFSET_WIN_AMMO_LMG			1116
-#define OFFSET_LIN_AMMO_LMG			1136
+#define	OFFSET_WIN_AMMO_LMG			1116
+#define	OFFSET_LIN_AMMO_LMG			1136
 
-#define OFFSET_WIN_AMMO_PISTOL		1120
-#define OFFSET_LIN_AMMO_PISTOL		1140
+#define	OFFSET_WIN_AMMO_PISTOL		1120
+#define	OFFSET_LIN_AMMO_PISTOL		1140
 
-#define OFFSET_WIN_AMMO_SHOTGUN		1124
-#define OFFSET_LIN_AMMO_SHOTGUN		1144
+#define	OFFSET_WIN_AMMO_SHOTGUN		1124
+#define	OFFSET_LIN_AMMO_SHOTGUN		1144
 
-#define OFFSET_WIN_AMMO_HMG			1128
-#define OFFSET_LIN_AMMO_HMG			1148
+#define	OFFSET_WIN_AMMO_HMG			1128
+#define	OFFSET_LIN_AMMO_HMG			1148
 
-#define OFFSET_WIN_AMMO_GL			1132
-#define OFFSET_LIN_AMMO_GL			1152
+#define	OFFSET_WIN_AMMO_GL			1132
+#define	OFFSET_LIN_AMMO_GL			1152
 
-#define OFFSET_WIN_AMMO_HG			1136
-#define OFFSET_LIN_AMMO_HG			1156
+#define	OFFSET_WIN_AMMO_HG			1136
+#define	OFFSET_LIN_AMMO_HG			1156
 
-#define OFFSET_WIN_DEATHS			1380
-#define OFFSET_LIN_DEATHS			1400
+#define	OFFSET_WIN_DEATHS			1380
+#define	OFFSET_LIN_DEATHS			1400
 
-#define OFFSET_WIN_STRUCTOWNER		324
-#define OFFSET_LIN_STRUCTOWNER		340
+#define	OFFSET_WIN_STRUCTOWNER		324
+#define	OFFSET_LIN_STRUCTOWNER		340
 
-#define OFFSET_WIN_HIVEABILITY		6248
-#define OFFSET_LIN_HIVEABILITY		6268
+#define OFFSET_WIN_HIVEABILITY		6268	//6248
+#define	OFFSET_LIN_HIVEABILITY		6336	//6268
 
 #define OFFSET_WIN_PLAYER_ITEMS		1068
 #define OFFSET_LIN_PLAYER_ITEMS		1088
 
-#define OFFSET_WIN_NEXT_WEAPON		1280 
+#define OFFSET_WIN_NEXT_WEAPON		1280	//2023 - unused? idk what this is supposed to be
 #define OFFSET_LIN_NEXT_WEAPON		1300
 
 #define OFFSET_WIN_CURRENT_WEAPON	1092
-#define OFFSET_LIN_CURRENT_WEAPON	1112 // +4 is the client known weapon if it's ever needed
+#define OFFSET_LIN_CURRENT_WEAPON	1112	// +4 is the client known weapon if it's ever needed
 
 #define OFFSET_WIN_LAST_WEAPON		1100
 #define OFFSET_LIN_LAST_WEAPON		1120
@@ -117,31 +117,35 @@
 #define OFFSET_WIN_ENTVAR			4
 #define OFFSET_LIN_ENTVAR			4 // first entry in the virtual class (vtable is 0)
 
-#define OFFSET_WIN_OBS_ENERGY		476
-#define OFFSET_LIN_OBS_ENERGY		492
+#define OFFSET_WIN_OBS_ENERGY		500		//could be 596 as fuser1 instead of mEnergy	//old 476 
+#define OFFSET_LIN_OBS_ENERGY		516		//could be 596	//old 492
 
-#define OFFSET_WIN_WELD_DONE		128
-#define OFFSET_LIN_WELD_DONE		144
+#define OFFSET_WIN_WELD_DONE		164		//128
+#define OFFSET_LIN_WELD_DONE		180		//144
 
-#define OFFSET_WIN_WELD_TIME		132
-#define OFFSET_LIN_WELD_TIME		148
+#define OFFSET_WIN_WELD_TIME		168		//132
+#define OFFSET_LIN_WELD_TIME		184		//148
 
-#define OFFSET_WIN_GHOST_STRUCTURE	472
-#define OFFSET_LIN_GHOST_STRUCTURE	488
+#define OFFSET_WIN_GHOST_STRUCTURE	497		//could be 592 as iuser4 instead of mGhost	//old 472
+#define OFFSET_LIN_GHOST_STRUCTURE	513		//could be 592	//old 488
 
 // The teamnumber for the specified team
-#define OFFSET_WIN_GAMEPLAY_TEAMA	0x54
-#define OFFSET_LIN_GAMEPLAY_TEAMA	0x64
+#define OFFSET_WIN_GAMEPLAY_TEAMA	0x54	//same  // bottom 2 this+offset in ?GetTeamNumber@AvHTeam@@QBE?AW4AvHTeamNumber@@XZ ; AvHTeam::GetTeamNumber(void)
+#define OFFSET_LIN_GAMEPLAY_TEAMA	0x64	//could be 0x54		//__ZNK7AvHTeam13GetTeamNumberEv ; AvHTeam::GetTeamNumber(void)
 
-#define OFFSET_WIN_GAMEPLAY_TEAMB	0x58
-#define OFFSET_LIN_GAMEPLAY_TEAMB	0x68
+#define OFFSET_WIN_GAMEPLAY_TEAMB	0x58	//same
+#define OFFSET_LIN_GAMEPLAY_TEAMB	0x68	//could be 0x58		//__ZNK7AvHTeam13GetTeamNumberEv ; AvHTeam::GetTeamNumber(void)
 
 
-#define OFFSET_WIN_UPGRADES_BOUGHT	0x1980
-#define OFFSET_LIN_UPGRADES_BOUGHT	0x1994
+#define OFFSET_WIN_UPGRADES_BOUGHT	0x19B8	//old 0x1980	//mPurchasedCombatUpgrades or under AvHPlayer::GetPurchasedCombatUpgrades(AvHPlayer *this) in disassembler
+#define OFFSET_LIN_UPGRADES_BOUGHT	0x19FC	//old 0x1994	 //new 19FC 6652
 
-#define OFFSET_WIN_UPGRADES_ACTIVE	0x198C
-#define OFFSET_LIN_UPGRADES_ACTIVE	0x19A0
+#define OFFSET_WIN_UPGRADES_ACTIVE	0x19C4	//old 0x198C //mGiveCombatUpgrades
+#define OFFSET_LIN_UPGRADES_ACTIVE	0x1A08	//old 0x19A0 //new offset of mGiveCombatUpgrades is 6664
+
+#define OFFSET_WIN_COMBAT_TECHTREE	0x19D0	//old 0x1998 //in AvHPlayer::GetCombatNodes(AvHPlayer *this)
+#define OFFSET_LIN_COMBAT_TECHTREE	0x1A14	//old 0x19AC //new offset of mCombatNodes is 6676
+
 
 
 // TODO: Symbols / signatures instead
@@ -150,8 +154,8 @@
 #define OFFSET_WIN_BASE				"?SUB_CallUseToggle@CBaseEntity@@QAEXXZ"
 #define OFFSET_LIN_BASE				"_init"
 
-#define OFFSET_LIN_MEMBERFUNCSTART	0x000EDBE8  // The location of _init in the binary
-#define OFFSET_WIN_MEMBERFUNCSTART	0x00001000  // The location of CBaseEntity::SUB_CallUseToggle
+#define OFFSET_LIN_MEMBERFUNCSTART	0x000A1000	//0x000EDBE8  // The location of _init in the binary //located at "; _init"
+#define OFFSET_WIN_MEMBERFUNCSTART	0x00001A40	//old 0x00001000  // The location of CBaseEntity::SUB_CallUseToggle
 
 // NOTE: These addresses are raw offsets, not including the
 //       base addresses!  These are the exact offsets that 
@@ -160,25 +164,28 @@
 
 
 // Recycle:   void AvHBaseBuildable::StartRecycle(void)
-#define OFFSET_WIN_MEMBER_RECYCLE				MAKE_MEMBER_OFFSET(0x00053950)
-#define OFFSET_LIN_MEMBER_RECYCLE				MAKE_MEMBER_OFFSET(0x00180800)
+#define OFFSET_WIN_MEMBER_RECYCLE				MAKE_MEMBER_OFFSET(0x0006FB40)	//old MAKE_MEMBER_OFFSET(0x00053950)
+#define OFFSET_LIN_MEMBER_RECYCLE				MAKE_MEMBER_OFFSET(0x00131860)	//old MAKE_MEMBER_OFFSET(0x00180800) //AvHBaseBuildable::StartRecycle(AvHBaseBuildable *__hidden this)
 
 
 // Weldable:  void AvHWeldable::AddWeldTime(float Time)
-#define OFFSET_WIN_MEMBER_TRIGGER_WELDABLE		MAKE_MEMBER_OFFSET(0x000A0E20)
-#define OFFSET_LIN_MEMBER_TRIGGER_WELDABLE		MAKE_MEMBER_OFFSET(0x00232840)
+#define OFFSET_WIN_MEMBER_TRIGGER_WELDABLE		MAKE_MEMBER_OFFSET(0x000D65A0)	//old MAKE_MEMBER_OFFSET(0x000A0E20)
+#define OFFSET_LIN_MEMBER_TRIGGER_WELDABLE		MAKE_MEMBER_OFFSET(0x001D55D0)	//old MAKE_MEMBER_OFFSET(0x00232840)
 
-
+// Research:  bool AvHTechTree::SetResearchDone(AvHMessageID inMessageID, bool inState)
+#define OFFSET_WIN_MEMBER_SET_RESEARCH_DONE		MAKE_MEMBER_OFFSET(0x000D2EB0)	//old MAKE_MEMBER_OFFSET(0x0009EC40)
+#define OFFSET_LIN_MEMBER_SET_RESEARCH_DONE		MAKE_MEMBER_OFFSET(0x001CFD90)	//old MAKE_MEMBER_OFFSET(0x0022E740)
 
 // Game Rules: AvHGameRules *GetGameRules(void)
-#define OFFSET_WIN_GETGAMERULES					MAKE_MEMBER_OFFSET(0x00068000)
-#define OFFSET_LIN_GETGAMERULES					MAKE_MEMBER_OFFSET(0x0019F930)
+#define OFFSET_WIN_GETGAMERULES					MAKE_MEMBER_OFFSET(0x000847F0)	//old MAKE_MEMBER_OFFSET(0x00068000)
+#define OFFSET_LIN_GETGAMERULES					MAKE_MEMBER_OFFSET(0x00157A60)	//old MAKE_MEMBER_OFFSET(0x0019F930)
 
+//2023 - These were/are for windows but idk if linux is different.
 // Offset into the gamerules pointer to the TeamA / TeamB class
-#define GAMERULES_TEAMA_OFFSET					0xCC
-#define GAMERULES_TEAMB_OFFSET					0x2A8
+#define GAMERULES_TEAMA_OFFSET					0x0C8	//old 0xCC
+#define GAMERULES_TEAMB_OFFSET					0x2B4	//old 0x2A8
 
-#define AVHTEAM_RESOURCES_OFFSET				0x94
+#define AVHTEAM_RESOURCES_OFFSET				0x0AC		//old 0x94 //mTeamResources in AvHTeam::GetTeamResources 
 
 #define GAMERULES_TEAMA_RESOURCES				GAMERULES_TEAMA_OFFSET + AVHTEAM_RESOURCES_OFFSET
 #define GAMERULES_TEAMB_RESOURCES				GAMERULES_TEAMB_OFFSET + AVHTEAM_RESOURCES_OFFSET
@@ -193,62 +200,62 @@ enum
 	NSGame_MarineVMarine,	/**< Marine vs Marine */
 	NSGame_AlienVAlien,		/**< Alien  vs Alien  */
 
-	NSGame_Unknown,			/**< Can find the gameplay entity, but can't 
+	NSGame_Unknown,			/**< Can find the gameplay entity, but can't
 								 determine gameplay type. */
 };
 
 enum
 {
-   MASK_NONE = 0,
-   MASK_SIGHTED = 1,
-   MASK_DETECTED = 2,
-   MASK_BUILDABLE = 4,
-   MASK_BASEBUILD0 = 8, // Base build slot #0
-   MASK_WEAPONS1 = 8, // Marine weapons 1
-   MASK_CARAPACE = 8, // Alien carapace
-   MASK_WEAPONS2 = 16, // Marines weapons 2
-   MASK_REGENERATION = 16, // Alien regeneration
-   MASK_BASEBUILD1 = 16, // Base build slot #1
-   MASK_WEAPONS3 = 32, // Marine weapons 3
-   MASK_REDEMPTION = 32, // Alien redemption
-   MASK_BASEBUILD2 = 32, // Base build slot #2
-   MASK_ARMOR1 = 64, // Marine armor 1
-   MASK_CELERITY = 64, // Alien celerity
-   MASK_BASEBUILD3 = 64, // Base build slot #3
-   MASK_ARMOR2 = 128, // Marine armor 2
-   MASK_ADRENALINE = 128, // Alien adrenaline
-   MASK_BASEBUILD4 = 128, // Base build slot #4
-   MASK_ARMOR3 = 256, // Marine armor 3
-   MASK_SILENCE = 256, // Alien silence
-   MASK_BASEBUILD5 = 256, // Base build slot #5
-   MASK_JETPACK = 512, // Marine jetpacks
-   MASK_CLOAKING = 512, // Alien cloaking
-   MASK_BASEBUILD6 = 512, // Base build slot #6
-   MASK_FOCUS = 1024, // Alien focus
-   MASK_MOTION = 1024, // Marine motion tracking
-   MASK_BASEBUILD7 = 1024, // Base build slot #7
-   MASK_SCENTOFFEAR = 2048,   // Alien scent of fear
-   MASK_DEFENSE2 = 4096, // Defense level 2
-   MASK_DEFENSE3 = 8192, // Defense level 3
-   MASK_ELECTRICITY = 8192,   // Electricy
-   MASK_MOVEMENT2 = 16384, // Movement level 2,
-   MASK_MOVEMENT3 = 32768, // Movement level 3
-   MASK_HEAVYARMOR = 32768,   // Marine heavy armor
-   MASK_SENSORY2 = 65536, // Sensory level 2
-   MASK_SENSORY3 = 131072, // Sensory level 3
-   MASK_ALIEN_MOVEMENT = 262144,   // Onos is charging
-   MASK_WALLSTICKING = 524288,   // Flag for wall-sticking
-   MASK_PRIMALSCREAM = 1048576,   // Alien is in range of active primal scream
-   MASK_UMBRA = 2097152, // In umbra
-   MASK_DIGESTING = 4194304,   // When set on a visible player, player is digesting.  When set on invisible player, player is being digested
-   MASK_RECYCLING = 8388608,   // Building is recycling
-   MASK_TOPDOWN = 16777216,   // Commander view
-   MASK_PLAYER_STUNNED = 33554432,   // Player has been stunned by stomp
-   MASK_ENSNARED = 67108864,   // Webbed
-   MASK_ALIEN_EMBRYO = 134217728, //268435456,   // Gestating
-   MASK_SELECTABLE = 268435456, //536870912,   // ???
-   MASK_PARASITED = 536870912,    //1073741824,   // Parasite flag
-   MASK_SENSORY_NEARBY = 1073741824   //2147483648   // Sensory chamber in range
+	MASK_NONE = 0,
+	MASK_SIGHTED = 1,
+	MASK_DETECTED = 2,
+	MASK_BUILDABLE = 4,
+	MASK_BASEBUILD0 = 8, // Base build slot #0
+	MASK_WEAPONS1 = 8, // Marine weapons 1
+	MASK_CARAPACE = 8, // Alien carapace
+	MASK_WEAPONS2 = 16, // Marines weapons 2
+	MASK_REGENERATION = 16, // Alien regeneration
+	MASK_BASEBUILD1 = 16, // Base build slot #1
+	MASK_WEAPONS3 = 32, // Marine weapons 3
+	MASK_REDEMPTION = 32, // Alien redemption
+	MASK_BASEBUILD2 = 32, // Base build slot #2
+	MASK_ARMOR1 = 64, // Marine armor 1
+	MASK_CELERITY = 64, // Alien celerity
+	MASK_BASEBUILD3 = 64, // Base build slot #3
+	MASK_ARMOR2 = 128, // Marine armor 2
+	MASK_ADRENALINE = 128, // Alien adrenaline
+	MASK_BASEBUILD4 = 128, // Base build slot #4
+	MASK_ARMOR3 = 256, // Marine armor 3
+	MASK_SILENCE = 256, // Alien silence
+	MASK_BASEBUILD5 = 256, // Base build slot #5
+	MASK_JETPACK = 512, // Marine jetpacks
+	MASK_CLOAKING = 512, // Alien cloaking
+	MASK_BASEBUILD6 = 512, // Base build slot #6
+	MASK_FOCUS = 1024, // Alien focus
+	MASK_MOTION = 1024, // Marine motion tracking
+	MASK_BASEBUILD7 = 1024, // Base build slot #7
+	MASK_SCENTOFFEAR = 2048,   // Alien scent of fear
+	MASK_DEFENSE2 = 4096, // Defense level 2
+	MASK_DEFENSE3 = 8192, // Defense level 3
+	MASK_ELECTRICITY = 8192,   // Electricy
+	MASK_MOVEMENT2 = 16384, // Movement level 2,
+	MASK_MOVEMENT3 = 32768, // Movement level 3
+	MASK_HEAVYARMOR = 32768,   // Marine heavy armor
+	MASK_SENSORY2 = 65536, // Sensory level 2
+	MASK_SENSORY3 = 131072, // Sensory level 3
+	MASK_ALIEN_MOVEMENT = 262144,   // Onos is charging
+	MASK_WALLSTICKING = 524288,   // Flag for wall-sticking
+	MASK_PRIMALSCREAM = 1048576,   // Alien is in range of active primal scream
+	MASK_UMBRA = 2097152, // In umbra
+	MASK_DIGESTING = 4194304,   // When set on a visible player, player is digesting.  When set on invisible player, player is being digested
+	MASK_RECYCLING = 8388608,   // Building is recycling
+	MASK_TOPDOWN = 16777216,   // Commander view
+	MASK_PLAYER_STUNNED = 33554432,   // Player has been stunned by stomp
+	MASK_ENSNARED = 67108864,   // Webbed
+	MASK_ALIEN_EMBRYO = 134217728, //268435456,   // Gestating
+	MASK_SELECTABLE = 268435456, //536870912,   // ???
+	MASK_PARASITED = 536870912,    //1073741824,   // Parasite flag
+	MASK_SENSORY_NEARBY = 1073741824   //2147483648   // Sensory chamber in range
 };
 
 typedef enum
@@ -277,11 +284,11 @@ typedef enum
 	AVH_USER3_AUDIO_OFF,
 	AVH_USER3_FUNC_RESOURCE,
 	AVH_USER3_COMMANDER_STATION,
-	AVH_USER3_TURRET_FACTORY, 
-	AVH_USER3_ARMORY, 
+	AVH_USER3_TURRET_FACTORY,
+	AVH_USER3_ARMORY,
 	AVH_USER3_ADVANCED_ARMORY,
 	AVH_USER3_ARMSLAB,
-	AVH_USER3_PROTOTYPE_LAB, 
+	AVH_USER3_PROTOTYPE_LAB,
 	AVH_USER3_OBSERVATORY,
 	AVH_USER3_CHEMLAB,
 	AVH_USER3_MEDLAB,
